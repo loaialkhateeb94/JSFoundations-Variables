077ae6a1ed0b34311891b1b1eb84ee88c8800279
loaialkhateeb94 <loaialkhateeb94@gmail.com>
2021-05-17 15:51:33 +0300

    created story.js

---
+++ story.js
@@ -0,0 +1,6 @@
+let name = "Loay"
+let age = 27
+let hoppy = "Karting"
+
+console.log (`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hoppy} practice!`)
+
